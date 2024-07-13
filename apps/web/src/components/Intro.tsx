cd3eef834b61e8c70ea6f7c1c33c721c13e12901
Luke Oliff <luke@lukeoliff.com>
2024-07-13 16:07:57 +0100

    chore: fix intro message

--- apps/web/src/components/Intro.tsx
+++ apps/web/src/components/Intro.tsx
@@ -1,6 +1,4 @@
-import { IconLink } from '@/components/IconLink'
 import Link from 'next/link'
-import { SiGithub, SiLinkedin, SiX } from '@icons-pack/react-simple-icons'
 
 export function Intro() {
   return (
@@ -10,56 +8,6 @@ export function Intro() {
         <br />
         <span className="text-sky-300">Developer Experience</span>
       </h1>
-      <p className="mt-4 text-sm/6 text-gray-300">
-        I build visually stunning demonstration products, fast.
-      </p>
-      <p className="mt-4 text-sm/6 text-gray-300">
-        I am a seasoned Developer Experience Engineer with a rich background in
-        software development, focusing on security and open-source. Based in the
-        UK, I am an experienced remote worker with almost 8 years working for US
-        organisations remotely.
-      </p>
-      <p className="mt-4 text-sm/6 text-gray-300">
-        I am dedicated to enhancing developer tools and experiences, building
-        visually stunning product demos, writing accessible guides and
-        documentation.
-      </p>
-      <div className="mt-8 flex flex-wrap justify-center gap-x-1 gap-y-3 sm:gap-x-2 lg:justify-start">
-        <IconLink
-          href="https://twitter.com/lukeocodes"
-          target="_blank"
-          rel="noopener noreferrer"
-          icon={() => <SiX color="#ffffff" size={12} />}
-          className="flex-none"
-        >
-          Twitter
-        </IconLink>
-        <IconLink
-          href="https://www.linkedin.com/in/lukeocodes/"
-          target="_blank"
-          rel="noopener noreferrer"
-          icon={() => <SiLinkedin color="#0A66C2" size={12} />}
-          className="flex-none"
-        >
-          LinkedIn
-        </IconLink>
-        <IconLink
-          href="https://github.com/lukeocodes"
-          target="_blank"
-          rel="noopener noreferrer"
-          icon={() => <SiGithub color="#ffffff" size={12} />}
-          className="flex-none"
-        >
-          GitHub
-        </IconLink>
-
-        {/* <IconLink href="#" icon={GitHubIcon} className="flex-none">
-          GitHub
-        </IconLink>
-        <IconLink href="/feed.xml" icon={FeedIcon} className="flex-none">
-          RSS
-        </IconLink> */}
-      </div>
     </>
   )
 }
@@ -77,9 +25,6 @@ export function IntroFooter() {
         Luke Oliff
       </Link>
       . All rights reserved.
-      {/* <IconLink href="#" icon={XIcon} compact>
-        Joe Davola
-      </IconLink> */}
     </p>
   )
 }
